63735922a06a3e44083952fdc14039ebc96bfe99
paixaas <lmadureirapaixao@gmail.com>
2026-04-08 20:07:45 -0300

    Update route.ts

--- app/webapi/webhook/route.ts
+++ app/webapi/webhook/route.ts
@@ -1,22 +1,26 @@
 import { NextResponse } from 'next/server';
 
-const DISCORD_WEBHOOK_ID = process.env.DISCORD_WEBHOOK_ID!;
+const DISCORD_URL = process.env.DISCORD_WEBHOOK_URL!; 
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    await fetch(`https://discord.com/api/webhooks/${DISCORD_WEBHOOK_ID}`, {
+    const response = await fetch(DISCORD_URL, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
     });
 
+    if (!response.ok) {
+      const errorData = await response.text();
+      console.error('O Discord recusou o webhook:', errorData);
+      return NextResponse.json({ ok: false, error: errorData }, { status: response.status });
+    }
+
     return NextResponse.json({ ok: true });
   } catch (err) {
-    console.error('Erro no proxy:', err);
+    console.error('Erro interno no Proxy:', err);
     return NextResponse.json({ ok: false }, { status: 500 });
   }
 }
