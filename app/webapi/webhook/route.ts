7de12fdbdaffc84a881ff9130aa00ef17bea08a4
paixaas <lmadureirapaixao@gmail.com>
2026-04-08 20:12:40 -0300

    Update route.ts

--- app/webapi/webhook/route.ts
+++ app/webapi/webhook/route.ts
@@ -13,14 +13,10 @@ export async function POST(req: Request) {
     });
 
     if (!response.ok) {
-      const errorData = await response.text();
-      console.error('O Discord recusou o webhook:', errorData);
       return NextResponse.json({ ok: false, error: errorData }, { status: response.status });
     }
-
     return NextResponse.json({ ok: true });
   } catch (err) {
-    console.error('Erro interno no Proxy:', err);
     return NextResponse.json({ ok: false }, { status: 500 });
   }
 }
